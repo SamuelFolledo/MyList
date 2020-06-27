7f6d927842d5b9a84893394719cd43d7e2d79fb8
Samuel Folledo <samuelfolledo@gmail.com>
2020-06-26 22:23:01 -0400

    [add] removed storyboard

--- MyList/SceneDelegate.swift
+++ MyList/SceneDelegate.swift
@@ -11,13 +11,17 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    var navigationController: UINavigationController?
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: UIScreen.main.bounds) //needed to not use storyboard
+        let vc = ViewController() //needed to not use storyboard
+        navigationController = UINavigationController(rootViewController: vc)
+        window?.rootViewController = navigationController //needed to not use storyboard
+        window?.makeKeyAndVisible() //needed to not use storyboard
+        window?.windowScene = windowScene //needed to not use storyboard
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
