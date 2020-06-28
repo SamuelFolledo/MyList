621f0befa750ef49be81ce1e5e0195bb7d947eca
Samuel Folledo <samuelfolledo@gmail.com>
2020-06-28 04:00:31 -0400

    [add] cleaner

--- MyList/CoreData/CoreDataStack.swift
+++ MyList/CoreData/CoreDataStack.swift
@@ -10,35 +10,37 @@ import Foundation
 import CoreData
 
 class CoreDataStack {
-
-  private let modelName: String
-
-  init(modelName: String) {
-    self.modelName = modelName
-  }
-
-  lazy var managedContext: NSManagedObjectContext = {
-    return self.storeContainer.viewContext
-  }()
-
-  private lazy var storeContainer: NSPersistentContainer = {
-
-    let container = NSPersistentContainer(name: self.modelName)
-    container.loadPersistentStores { (storeDescription, error) in
-      if let error = error as NSError? {
-        print("Unresolved error \(error), \(error.userInfo)")
-      }
+    
+    private let modelName: String
+    
+    init(modelName: String) {
+        self.modelName = modelName
     }
-    return container
-  }()
+    
+    lazy var mainContext: NSManagedObjectContext = {
+        return self.storeContainer.viewContext
+    }()
+    
+    private lazy var storeContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: self.modelName)
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                print("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+}
 
-  func saveContext () {
-    guard managedContext.hasChanges else { return }
-
-    do {
-      try managedContext.save()
-    } catch let error as NSError {
-      print("Unresolved error \(error), \(error.userInfo)")
+// MARK: Internal
+extension CoreDataStack {
+    func saveContext () {
+        guard mainContext.hasChanges else { return }
+        
+        do {
+            try mainContext.save()
+        } catch let nserror as NSError {
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
     }
-  }
 }
