6b6136674fcf163ac529f59755daf5b4f5e5cf40
Samuel Folledo <samuelfolledo@gmail.com>
2020-06-29 03:51:06 -0400

    [add] created colorView

--- MyList/Views/ColorView.swift
+++ MyList/Views/ColorView.swift
@@ -9,13 +9,40 @@
 import UIKit
 
 class ColorView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    enum Shape {
+        case round, square
     }
-    */
-
+    
+    let shape: Shape
+    let color: UIColor
+    let isFilled: Bool
+    
+    //init with parameters
+    required init(shape: Shape, color: UIColor, isFilled: Bool) {
+        self.shape = shape
+        self.color = color
+        self.isFilled = isFilled
+        super.init(frame: .zero)
+        setupView()
+    }
+    
+    
+    //initWithCode to init view from xib or storyboard
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    fileprivate func setupView() {
+        layer.masksToBounds = true
+        switch shape {
+        case .round:
+            layer.cornerRadius = 20
+        case .square:
+            layer.cornerRadius = 5
+        }
+        layer.borderColor = color.cgColor
+        layer.borderWidth = 2
+        backgroundColor = isFilled ? color : .white
+    }
 }
