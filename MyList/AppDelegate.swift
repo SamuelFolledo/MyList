c4c04439095ed41187160b1971720b9e5dfdcb8f
Samuel Folledo <samuelfolledo@gmail.com>
2020-07-01 01:47:32 -0400

    [add] put coreDataStack to AppDelegate

--- MyList/AppDelegate.swift
+++ MyList/AppDelegate.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    let coreDataStack = CoreDataStack(modelName: "MyList")
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -31,3 +34,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 }
 
+//MARK: CoreData Methods
+extension AppDelegate {
+    func saveContext() {
+        coreDataStack.saveContext()
+    }
+}
