d89e8f98a26b06dd448ea0d27c204f117cef78f5
Samuel Folledo <samuelfolledo@gmail.com>
2020-07-01 16:42:36 -0400

    [add] date extensions

---
+++ MyList/Helpers/Extensions/Date+Extensions.swift
@@ -0,0 +1,38 @@
+//
+//  Date+Extensions.swift
+//  MyList
+//
+//  Created by Samuel Folledo on 7/1/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    struct Formatter {
+        static let utcFormatter: DateFormatter = {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss'Z'"
+            dateFormatter.timeZone = TimeZone(identifier: "EST") //load the user's current TimeZone identifier here
+            return dateFormatter
+        }()
+    }
+    
+    var dateToUTC: String {
+        return Formatter.utcFormatter.string(from: self)
+    }
+}
+
+extension String {
+    struct Formatter {
+        static let utcFormatter: DateFormatter = {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ssz"
+            return dateFormatter
+        }()
+    }
+    
+    var dateFromUTC: Date? {
+        return Formatter.utcFormatter.date(from: self)
+    }
+}
