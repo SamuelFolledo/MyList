a0880268a14cae26545a13e6e3131e9bb4ab861b
Samuel Folledo <samuelfolledo@gmail.com>
2020-07-04 03:46:52 -0400

    [add] a toDoTasks and doneTasks properties

--- Project+CoreDataClass.swift
+++ Project+CoreDataClass.swift
@@ -17,10 +17,26 @@ public class Project: NSManagedObject {
 
 extension Project {
     ///to section each project by day of lastOpenedDate
-    @objc var isoDate: String { get {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "d MMMM yyyy"
-        // insert settings for TimeZone and Calendar here
-        return formatter.string(from: self.lastOpenedDate)
-    }}
+    @objc var isoDate: String {
+        get {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "d MMMM yyyy"
+            // insert settings for TimeZone and Calendar here
+            return formatter.string(from: self.lastOpenedDate)
+        }
+    }
+    
+    @objc var toDoTasks: [Task] {
+        get {
+            guard let tasks = tasks.mutableCopy() as? [Task] else { return [] } //convert tasks to array of tasks
+            return tasks.filter() { !$0.isDone } //return unfinished tasks
+        }
+    }
+    
+    @objc var doneTasks: [Task] {
+        get {
+            guard let tasks = tasks.mutableCopy() as? [Task] else { return [] } //convert tasks to array of tasks
+            return tasks.filter() { $0.isDone } //return finished tasks
+        }
+    }
 }
