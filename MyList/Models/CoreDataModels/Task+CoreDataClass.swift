0210b3bc274dd0f06f524f0ebac7b7e8f4a3e18f
Samuel Folledo <samuelfolledo@gmail.com>
2020-07-16 05:10:03 -0400

    [add] dateStatus

--- MyList/Models/CoreDataModels/Task+CoreDataClass.swift
+++ MyList/Models/CoreDataModels/Task+CoreDataClass.swift
@@ -12,5 +12,23 @@ import CoreData
 
 @objc(Task)
 public class Task: NSManagedObject {
+    
+}
 
+extension Task {
+    @objc var overDueStatus: String {
+        get {
+            if self.dueDate! < Date() { //if current date is greater than due date, it's overDue
+                return "Overdue Tasks 🚨‼️"
+            } else { //for upcoming tasks
+                if Calendar.current.isDateInToday(dueDate!) { //if task is due today
+                    return "Due Today"
+                } else if Calendar.current.isDateInTomorrow(dueDate!) { //if due tomorrow
+                    return "Due Tomorrow"
+                } else { //if due past tomorrow
+                    return "Upcoming Tasks"
+                }
+            }
+        }
+    }
 }
