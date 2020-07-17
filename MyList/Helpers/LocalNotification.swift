1951113dc553cd016888733901384ed624986efc
Samuel Folledo <samuelfolledo@gmail.com>
2020-07-17 18:16:05 -0400

    [add] LocalNotification Manager with request local notification permission and creating local notificaitons

--- MyList/Helpers/LocalNotification.swift
+++
@@ -1,9 +0,0 @@
-//
-//  LocalNotification.swift
-//  MyList
-//
-//  Created by Samuel Folledo on 7/17/20.
-//  Copyright © 2020 SamuelFolledo. All rights reserved.
-//
-
-import Foundation
---
+++ MyList/Helpers/LocalNotificationManager.swift
@@ -0,0 +1,86 @@
+//
+//  LocalNotificationManager.swift
+//  MyList
+//
+//  Created by Samuel Folledo on 7/17/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit.UNNotificationResponse_UIKitAdditions
+
+struct LocalNotificationManager {
+    
+    ///asks permission to push local notication
+    static func requestLocalNotification(completion: @escaping (_ error: String?, _ granted: Bool) -> Void) {
+        checkPermision { (granted) in
+            if granted {
+                completion(nil, granted)
+            } else { //if not permitted then ask for permission
+                let center = UNUserNotificationCenter.current()
+                center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+                    guard error == nil else {
+                        completion(error!.localizedDescription, granted)
+                        return
+                    }
+                    completion(nil, granted)
+                }
+            }
+        }
+    }
+    
+    ///Add local notification after checking permissions
+    static func schedule(title: String, message: String, userInfo: [AnyHashable: Any], task: Task, dueDate: Date,
+                              completion: @escaping (_ error: String?) -> Void) {
+        checkPermision { (granted) in
+            if granted {
+                create(title: title, message: message, userInfo: userInfo, task: task, dueDate: dueDate)
+                completion(nil)
+            } else {
+                requestLocalNotification { (error, granted) in
+                    guard error == nil else {
+                        completion(error!)
+                        return
+                    }
+                    if granted {
+                        create(title: title, message: message, userInfo: userInfo, task: task, dueDate: dueDate)
+                        completion(nil)
+                    } else {
+                        completion("Notification is disabled. Please go to Settings and allow us to send you notifications.")
+                    }
+                }
+            }
+        }
+    }
+    
+    //MARK: Private Helpers
+    private static func checkPermision(completion: @escaping (_ granted: Bool) -> Void) {
+        UNUserNotificationCenter.current().getNotificationSettings() { (settings) in
+            switch settings.alertSetting {
+            case .enabled:
+                completion(true)
+            case .disabled, .notSupported:
+                completion(false)
+            default:
+                break
+            }
+        }
+    }
+    
+    private static func create(title: String, message: String, userInfo: [AnyHashable: Any], task: Task, dueDate: Date) {
+        let center = UNUserNotificationCenter.current()
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = message
+        content.categoryIdentifier = "alarm" //custom actions
+        content.userInfo = userInfo //custom data to notification
+        content.sound = UNNotificationSound.default
+        //NOTE: Trigger by date
+        let dateComponents = Calendar.autoupdatingCurrent.dateComponents([.year, .month, .day, .hour, .minute], from: dueDate) //convert dueDate to calendar
+        print("Alert at \(dateComponents)")
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        //NOTE: Trigger by seconds
+//        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier: "\(task.project!.name)+\(task.name!)", content: content, trigger: trigger)
+        center.add(request)
+    }
+}
